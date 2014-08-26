747158dc82bd83b31a0f7b99c82f96d8cd79f5aa
Matthias Wiesner <mw@cloudcontrol.de>
2014-08-26 10:58:32 +0200

    Wip

--- instance.py
+++ instance.py
@@ -1,15 +1,13 @@
-import httplib2
 import time
+import httplib2
 
 from apiclient.discovery import build as discovery_build
 from json import dumps as json_dumps
 from oauth2client.client import SignedJwtAssertionCredentials
 
-CLIENT_SECRETS_FILE = 'client_secret.json'
-CREDENTIALS_FILE = 'credentials.json'
 
 SCOPE_SQLSERVICE = 'https://www.googleapis.com/auth/sqlservice.admin'
-SCOPE_PLATFORM = 'https://www.googleapis.com/auth/cloud-platform'
+PROJECT_ID = 'titanpaas'
 
 
 def get_authenticated_service():
@@ -24,31 +22,47 @@ def get_authenticated_service():
 
 
 def create_instance(service, instance, tier="D0"):
-    req = service.instances().insert(project="titanpaas", body={"project": "titanpaas", "instance": instance, "settings": {"tier": tier}})
+    req = service.instances().insert(project=PROJECT_ID, body={
+        "project": PROJECT_ID,
+        "instance": instance,
+        "settings": {
+            "tier": tier,
+            "ipConfiguration": {
+                "authorizedNetworks": ["0.0.0.0/0"],
+                "enabled": True
+            }}})
     resp = req.execute()
 
-    req = service.operations().get(project="titanpaas", instance=instance, operation=resp['operation'])
+    req = service.operations().get(project=PROJECT_ID, instance=instance, operation=resp['operation'])
     resp = req.execute()
     print json_dumps(resp, indent=2)
     for _ in range(60):
-        req = service.operations().get(project="titanpaas", instance=instance, operation=resp['operation'])
+        req = service.operations().get(project=PROJECT_ID, instance=instance, operation=resp['operation'])
         resp = req.execute()
         print json_dumps(resp, indent=2)
         if resp['state'] == 'DONE':
+            req = service.instances().setRootPassword(project=PROJECT_ID, instance=instance, body={
+                "setRootPasswordContext": {
+                    "password": "verysecret",
+                    "kind": "sql#setRootUserContext"
+                }
+            })
+            resp = req.execute()
+            print json_dumps(resp, indent=2)
             break
         time.sleep(1)
 
 
 def delete_instance(service, instance):
-    req = service.instances().delete(project="titanpaas", instance=instance)
-    resp = req.execute()
-
+    req = service.instances().delete(project=PROJECT_ID, instance=instance)
+    _resp = req.execute()
+    # TODO check if its done
 
 if __name__ == '__main__':
     service = get_authenticated_service()
 
-    delete_instance(service, "mkwtest5")
-    create_instance(service, "mkwtest6", tier="D2")
+    delete_instance(service, "mkwtest11")
+    create_instance(service, "mkwtest12", tier="D2")
     req = service.instances().list(project='titanpaas')
     resp = req.execute()
     print json_dumps(resp, indent=2)
